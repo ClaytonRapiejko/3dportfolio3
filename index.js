6b0ddd8b4f8e8e96f1b6f3d46bf06614648fffdd
Clayton Rapiejko <83980609+ClaytonRapiejko@users.noreply.github.com>
2022-01-28 16:02:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+const canvas = document.querySelector('canvas.webgl')
 
 // Setup
 
