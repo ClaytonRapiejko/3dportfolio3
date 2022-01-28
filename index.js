a0493f018913f99256aef2baa9e1b50ab33f9a9e
Clayton Rapiejko <claytonrapiejko@gmail.com>
2022-01-28 16:23:16 -0500

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import './style.css';
-import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+import { OrbitControls } from 'https://unpkg.com/browse/three@0.133.1/examples/js/controls/OrbitControls.js'
 
 // Setup
 
