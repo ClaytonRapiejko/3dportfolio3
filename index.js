a03976a4d06cc1cecf65064fcb1e2f0f8a368c70
Clayton Rapiejko <83980609+ClaytonRapiejko@users.noreply.github.com>
2022-01-28 19:41:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'https://unpkg.com/three@0.120.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.120.1/jsm/controls/OrbitControls';
 
 // Setup
 
